c655c275f49185f2de683214c64a3da7a757d499
Cooper D'Andilly <151992386+CooperDActor-bytes@users.noreply.github.com>
2025-06-18 14:50:23 +1000

    Add files via upload

---
+++ CircleNEWFlip/javas/anainout.js
@@ -0,0 +1,17 @@
+chrome.action.onClicked.addListener(function(tab) {
+    chrome.tabs.create({ 
+url: "index.html"
+ });
+});
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.tabs.create({ url: "index.html"});
+    chrome.tabs.create({ url: "https://724fun.com/circle-flip/"});
+});
+
+
+if(chrome.runtime.setUninstallURL) {
+  chrome.runtime.setUninstallURL('https://724fun.com/');
+} else {
+}
+
+
